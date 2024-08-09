9193c7b213ea28f3eac2fdb21da148add42a7fcf
Cahad <cahadrehimli7@gmail.com>
2024-08-09 16:40:26 +0400

    hi

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
         </div>
         <div className="hidden md:flex space-x-4 font-semibold">
           <a className="text-black hover:text-[#E85F5C] cursor-pointer">Home</a>
-          <a className="text-black opacity-35 hover:text-opacity-75 cursor-pointer">Products</a>
+          <a className="text-black opacity-35 hover:text-opacity-75 cursor-pointer">Search</a>
           <a className="text-black opacity-35 hover:text-opacity-75 cursor-pointer">About</a>
           <a className="text-black opacity-35 hover:text-opacity-75 cursor-pointer">Contacts</a>
         </div>
